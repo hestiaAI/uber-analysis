9fc8598e3217fb8311aefd9221c85bc112fa2454
Luã Streit <lua.streit@protonmail.com>
2022-12-08 09:58:22 +0100

    added types

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from typing import Dict
 import pandas as pd
 
-def save_to_excel(path, sheets):
+
+def save_to_excel(path: str, sheets: dict[str, pd.DataFrame]):
     with pd.ExcelWriter(path) as writer:
         for name, frame in sheets.items():
             frame.to_excel(writer, sheet_name=name)
-    print('saved ', path)
+    print(f'saved excel at {path}')
